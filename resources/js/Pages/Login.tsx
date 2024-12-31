0255b3612d8a396f5e08c6edbf90eac07a2ff9c4
Siraj Yesuf <sirajyesuf762@gmail.com>
2024-12-31 12:43:33 +0300

    =inertiajs installation moved from js to ts and shadcnui installed

---
+++ resources/js/Pages/Login.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+export default function Login(){
+
+    console.log("hi ")
+    return (
+      <div>
+      <Button>Click me</Button>
+    </div>
+    )
+}
+
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,7 @@
 
     <!-- Scripts -->
     @viteReactRefresh
-    @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+    @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
     @inertiaHead
 </head>
 
